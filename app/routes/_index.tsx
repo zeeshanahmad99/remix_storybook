bb8dc0783bfb5a164a92ddf2991cb6b6372ad2d8
Zeeshan Ahmad <zeems1234@gmail.com>
2023-11-28 12:10:09 +0000

    Initial commit

---
+++ app/routes/_index.tsx
@@ -0,0 +1,19 @@
+import type { MetaFunction } from "@remix-run/node";
+import { Welcome } from "~/components/Welcome/Welcome";
+import { ColorSchemeToggle } from "~/components/ColorSchemeToggle/ColorSchemeToggle";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Mantine Remix App" },
+    { name: "description", content: "Welcome to Mantine!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div>
+      <Welcome />
+      <ColorSchemeToggle />
+    </div>
+  );
+}
